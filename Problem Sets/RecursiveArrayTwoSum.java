bf2f289851823540f1c7ebd2a8a69311b5f50285
J-Mojica <joseleonardomojica@gmail.com>
2020-09-08 16:29:38 -0400

    Started RecursiveArrayTwoSum

---
+++ Problem Sets/RecursiveArrayTwoSum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/**
+ * The program RecursiveArrayTwoSum implements a recursive algorithm 
+ * that determines if an array of integers contains an integer that 
+ * is the sum of two integers appearing earlier in the array.
+ *
+ * @author Jose Mojica Perez
+ * @version 1.0
+ * @since 2020-09-07
+ */
+public class RecursiveArrayTwoSum{
+	public static void main(String[] args){
+		int defaultIndex = 0;
+		int[] arr = {1, 2, 3};
+		System.out.println(arr, dafaultIndex);
+	}
+	public static boolean FindTwoSum (int[] arr, int target, int index){
+		if(target - arr[index])
+			return true;
+		return true;
+	}
+}
