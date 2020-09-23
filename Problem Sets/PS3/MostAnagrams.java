5071af29af08ce4f6cf8e50b54453d0dde7392ba
J-Mojica <joseleonardomojica@gmail.com>
2020-09-23 00:42:10 -0400

    Sorted output alphabetically, added time complexity comment

--- Problem Sets/PS3/MostAnagrams.java
+++ Problem Sets/PS3/MostAnagrams.java
@@ -4,18 +4,28 @@ import java.io.File;
 import java.io.FileNotFoundException;
 /**
  * The program MostAnagrams finds the word with the largest number of anagrams
- * within a given number of lines from the text file words.txt
+ * within n lines from the text file words.txt, where n is a given positive integer
  *
  * words.txt contains words separated by the new line character
  *
  * The program outputs the largest number of anagrams found followed by a list of the
  * words that have that number of anagrams, ordered alphabetically
- *
+ * 
  * @author Jose Mojica Perez
  * @version 1.0
  * @since 2020-09-23
  *
  */
+
+/* The program compares iterates over n words, comparing each word to all n words, 
+ * finding the anagrams. If we let m be the maximum length of the words, then to find 
+ * the anagrams we iterate over at most m characters twice, and then we iterate over 
+ * 26 character frequencies to see if word_a has the same character frequencies as word_b,
+ * thus making it an anagram of word_a. This would give us a time complexity of:
+ *
+ * 	O(n^2 + 2m*n^2 + 26n^2) = O(2m*n^2 + 27n^2)
+ */
+
 public class MostAnagrams{
 	
 	public static void main(String[] args){
@@ -27,18 +37,22 @@ public class MostAnagrams{
 		ArrayList<String> rtn = new ArrayList<String>();
 		
 		try{
-			Scanner reader_lines = new Scanner(new File(file_name));
+			Scanner reader_1 = new Scanner(new File(file_name));
 			for(int i = 0; i < lines; i++){
-				String word_a = reader_lines.nextLine();
+				if(!reader_1.hasNextLine())
+					break;
+				String word_a = reader_1.nextLine();
 				char[] chars_a = word_a.toCharArray();	
 
 				ArrayList<String> temp = new ArrayList<String>();
-				Scanner reader = new Scanner(new File(file_name));
+				Scanner reader_2 = new Scanner(new File(file_name));
 				int anagram_count = 0;
 
 				for(int p = 0; p < lines; p++){
+					if(!reader_2.hasNextLine())
+						break;
 					boolean isAnagram = true;
-					String word_b = reader.nextLine();
+					String word_b = reader_2.nextLine();
 					char[] chars_b = word_b.toCharArray();
 					
 					int[] char_frequency = new int[26];
@@ -64,15 +78,16 @@ public class MostAnagrams{
 				if(temp.size() > rtn.size()){
 					rtn = temp;
 				}
-				reader.close();
+				reader_2.close();
 			}
 
-			reader_lines.close();
+			reader_1.close();
 		}
 		catch(FileNotFoundException e){
 			e.printStackTrace();
 		}
 		System.out.println(rtn.size());
+		rtn.sort(String::compareTo);
 		for(int n = 0; n < rtn.size(); n++){
 			System.out.println(rtn.get(n));
 		}
